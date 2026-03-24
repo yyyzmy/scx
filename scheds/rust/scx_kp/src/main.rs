85d6b50630406ea21c19b9f01d7963bf8c627d40
ye yiyang <850219375@qq.com>
2026-03-24 19:49:01 +0800

    fix scx_kp

--- scheds/rust/scx_kp/src/main.rs
+++ scheds/rust/scx_kp/src/main.rs
@@ -61,8 +61,8 @@ impl<'a> Scheduler<'a> {
         let mut skel_builder = BpfSkelBuilder::default();
         skel_builder.obj_builder.debug(opts.verbose);
         let open_opts = opts.libbpf.clone().into_bpf_open_opts();
-        let skel = scx_ops_open!(skel_builder, open_object, beerland_ops, open_opts)?;
-        let skel = scx_ops_load!(skel, beerland_ops, uei)?;
+        let mut skel = scx_ops_open!(skel_builder, open_object, beerland_ops, open_opts)?;
+        let mut skel = scx_ops_load!(skel, beerland_ops, uei)?;
         let struct_ops = Some(scx_ops_attach!(skel, beerland_ops)?);
 
         Ok(Self { skel, struct_ops })
